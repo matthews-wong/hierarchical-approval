9238a4a7d8fe0d163af4d260d4ae3eb7f8d212e6
Matthews Wong <122866494+matthews-wong@users.noreply.github.com>
2026-07-31 07:36:53 +0700

    feat(utils): add fatal level to Logger interface

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -2,6 +2,7 @@ export interface Logger {
   info(msg: string, context?: Record<string, unknown>): void;
   warn(msg: string, context?: Record<string, unknown>): void;
   error(msg: string, err?: unknown, context?: Record<string, unknown>): void;
+  fatal(msg: string, err?: unknown, context?: Record<string, unknown>): void;
   debug(msg: string, context?: Record<string, unknown>): void;
 }
 
@@ -9,5 +10,6 @@ export const noopLogger: Logger = {
   info: () => {},
   warn: () => {},
   error: () => {},
+  fatal: () => {},
   debug: () => {},
 };
