47f00e166d769160c16be9cd6a25fac718d7267c
Matthews Wong <122866494+matthews-wong@users.noreply.github.com>
2026-08-02 07:32:55 +0700

    feat(utils): export IdGeneratorPrefix type

--- src/utils/IdGenerator.ts
+++ src/utils/IdGenerator.ts
@@ -1,4 +1,6 @@
-export type IdGeneratorFn = (prefix: 'inst' | 'tpl') => string;
+export type IdGeneratorPrefix = 'inst' | 'tpl';
+
+export type IdGeneratorFn = (prefix: IdGeneratorPrefix) => string;
 
 export const defaultIdGenerator: IdGeneratorFn = (prefix) =>
   `${prefix}_${Date.now().toString(36)}_${Math.random().toString(36).slice(2, 10)}`;
